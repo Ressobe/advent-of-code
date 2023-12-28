92874a347e55643669a1099eaf7e149d25aefeea
Bartosz Sobina <bartosz.sobina44@gmail.com>
2023-12-28 11:54:33 +0100

    add solution for day1 part two 2019

--- 2019/day1/main.go
+++ 2019/day1/main.go
@@ -1,5 +1,6 @@
 package main
 
+
 import (
   "fmt"
   "math"
@@ -8,16 +9,17 @@ import (
   "strconv"
 )
 
+
 func calculateFuel(number float64) int {
   return int(math.Floor(number / 3)) - 2
 }
 
-func main() {
 
+func partOne() int {
   file, err := os.Open("input.txt")
   if err != nil {
     fmt.Println("Error opening file: ", err)
-    return
+    return -1
   }
   defer file.Close()
 
@@ -31,10 +33,53 @@ func main() {
 
       if err != nil {
         fmt.Println("Error while converting string to float", err)
-        return
+        return -1
       }
       sum += calculateFuel(number)
   }
+  return sum
+}
 
-  fmt.Println(sum)
+func calculateFuelPartTwo(number int) int {
+  fuel := 0
+  
+  for number >= 0 {
+    f := calculateFuel(float64(number))
+    if f >= 0 { fuel += f }
+    number = f
+  } 
+
+  return fuel
+}
+
+func partTwo() int {
+  file, err := os.Open("input.txt")
+  if err != nil {
+    fmt.Println("Error opening file: ", err)
+    return -1
+  }
+  defer file.Close()
+
+  var sum int
+
+	scanner := bufio.NewScanner(file)
+
+  for scanner.Scan() {
+      line := scanner.Text()
+      number, err := strconv.ParseFloat(line, 64)
+
+      if err != nil {
+        fmt.Println("Error while converting string to float", err)
+        return -1
+      }
+      sum += calculateFuelPartTwo(int(number))
+  }
+  return sum
+}
+
+
+func main() {
+  fmt.Println("Part one: ", partOne())
+  fmt.Println("Part two: ", partTwo())
+
 }
