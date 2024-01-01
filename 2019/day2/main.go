d3fc2f61eb0a02f5c28aaf3aba5d60bb46ce5706
Bartosz Sobina <bartosz.sobina44@gmail.com>
2024-01-01 18:08:48 +0100

    add solution for day2 part two 2019

--- 2019/day2/main.go
+++ 2019/day2/main.go
@@ -26,10 +26,18 @@ func program(line string) []int {
   return integers
 }
 
-func partOne() int {
+func readFile(filename string) (*os.File, error) {
   file, err := os.Open("input.txt")
   if err != nil {
-    fmt.Println("Error opening file: ", err)
+    return nil, err 
+  }
+  return file, nil
+}
+
+func partOne() int {
+  file, err := readFile("input.txt")
+  if err != nil {
+    fmt.Println("Error while reading input file")
     return -1
   }
   defer file.Close()
@@ -70,6 +78,60 @@ func partOne() int {
   return p[0]
 }
 
+func partTwo() int {
+  file, err := readFile("input.txt")
+  if err != nil {
+    fmt.Println("Error while reading input file")
+    return -1
+  }
+  defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+  scanner.Scan()
+  line := scanner.Text()
+
+  orginal_program := program(line)
+
+  p := make([]int, len(orginal_program))
+  copy(p, orginal_program)
+
+
+  for i := 0; i <= 100; i++  {
+    for j := 0; j <= 100; j++ {
+      p[1] = i
+      p[2] = j
+
+      instruction_pointer := 0
+      for instruction_pointer < len(p)  {
+        if p[instruction_pointer] == 99 {
+          break
+        }
+
+        num1_pos := p[instruction_pointer + 1] 
+        num2_pos := p[instruction_pointer + 2] 
+        output_pos := p[instruction_pointer + 3]
+
+        if p[instruction_pointer] == 1 {
+          p[output_pos] = p[num1_pos] + p[num2_pos]
+        }
+
+        if p[instruction_pointer] == 2 {
+          p[output_pos] = p[num1_pos] * p[num2_pos]
+        }
+        instruction_pointer += 4
+      }
+      if p[0] == 19690720 {
+        return 100 * p[1] + p[2]
+      }
+      // reset program
+      copy(p, orginal_program)
+    }
+  }
+     
+  return -1
+}
+
 func main() {
   fmt.Println(partOne())
+  fmt.Println(partTwo())
 }
